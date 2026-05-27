47909a5af5edcc512f8eb59aa17f402b94d18fa9
Namazuandmint <hayakura12@gmail.com>
2026-05-27 11:25:09 +0900

    s061

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import { useState } from 'react'
 import './lib/supabase'
 import './lib/groq'
 import './App.css'
 import { HomePage } from './pages/HomePage'
+import { FridgePage } from './pages/FridgePage'
 
 function App() {
-  return <HomePage />
+  const [currentPage, setCurrentPage] = useState<'home' | 'fridge'>('home')
+
+  return (
+    <>
+      {currentPage === 'home' ? (
+        <HomePage onNavigate={setCurrentPage} />
+      ) : (
+        <FridgePage onNavigate={setCurrentPage} />
+      )}
+    </>
+  )
 }
 
 export default App
